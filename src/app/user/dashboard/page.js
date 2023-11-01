5e25fc042841b68697c920f86269b7cd30a73c30
Shubham Dhiman <dhimanshubham440818@gmail.com>
2023-11-01 17:51:57 +0530

    how to use nested app routing in next js

---
+++ src/app/user/dashboard/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main>
+      <h1>User Dashboard with nested routing....</h1>
+     </main>
+  )
+}
---
+++ src/app/user/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+      <main>
+        <h1>User Page...</h1>
+       </main>
+    )
+  }  
